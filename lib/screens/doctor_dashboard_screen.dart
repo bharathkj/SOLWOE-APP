d943ff913b2f55c785664cb7c95e93467b5fa24c
Bharath <71270149+cerealsistaken@users.noreply.github.com>
2024-03-14 21:26:23 +0530

    made some things dynamic

--- lib/screens/doctor_dashboard_screen.dart
+++ lib/screens/doctor_dashboard_screen.dart
@@ -1,37 +1,72 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:solwoe/auth.dart';
-import 'package:solwoe/colors.dart';
-import 'package:solwoe/database.dart';
 import 'package:solwoe/model/appointment.dart';
-import 'package:solwoe/screens/appointment_screen.dart';
-import 'package:intl/intl.dart';
-import 'package:solwoe/screens/video_consultation_screen.dart';
+import 'package:solwoe/model/user.dart';
+import 'package:solwoe/screens/emotion_chart.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import 'emotion_chart.dart';
+import '../database.dart';
 
 class DoctorDashboardScreen extends StatefulWidget {
-  const DoctorDashboardScreen({super.key});
+  const DoctorDashboardScreen({Key? key}) : super(key: key);
 
   @override
-  State<DoctorDashboardScreen> createState() => _DoctorDashboardScreen();
+  State<DoctorDashboardScreen> createState() => _DoctorDashboardScreenState();
 }
 
-class _DoctorDashboardScreen extends State<DoctorDashboardScreen> {
-  String selectedEmail = "coomestofcoomer@gmail.com";
+class _DoctorDashboardScreenState extends State<DoctorDashboardScreen> {
+  //String selectedEmail = "coomestofcoomer@gmail.com";
+  String selectedPatientId = ""; // Initialize with empty string
+  List<String> patientIds = []; // List to store patient IDs
+
   @override
   void initState() {
     super.initState();
+    // Call the method to fetch the list of patient IDs in initState
+    fetchAssignedPatients();
   }
 
-  Widget _buildAppointmentsList(String status) {
+  // Method to fetch assigned patients
+  void fetchAssignedPatients() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser == null) {
+      throw Exception("User not logged in");
+    }
+
+    String? currentUserName = currentUser.displayName;
+    print("Current user name: $currentUserName");
+
+    // Fetch appointments where doctor_name is equal to the current user's name
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('appointments')
+        .where("doctor_name", isEqualTo: 'Dr. brian')
+        .get();
+
+    print("Number of documents returned: ${querySnapshot.docs.length}");
+
+    // Extract the list of patient IDs from the query snapshot
+    List<String> ids = querySnapshot.docs.map((doc) => doc['patient_id'] as String).toList();
+
+    setState(() {
+      // Update the patientIds list
+      patientIds = ids;
+      // Update the selectedPatientId with the first patient ID if available
+      if (patientIds.isNotEmpty) {
+        selectedPatientId = patientIds.first;
+      }
+    });
+
+    // Debug print to check patient IDs
+    print("Patient IDs: $patientIds");
+  }
+
+
+  Widget _buildAppointmentsList(String status, String currentuserdisplayname) {
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance
           .collection('appointments')
-          .where("doctor_name", isEqualTo: 'Dr. brian')
+          .where("doctor_name", isEqualTo: currentuserdisplayname)
           .orderBy("date", descending: true)
           .where('status', isEqualTo: status)
           .limit(5)
@@ -66,8 +101,7 @@ class _DoctorDashboardScreen extends State<DoctorDashboardScreen> {
             return ListTile(
               title: Text(appointment.patientName),
               subtitle: Text(
-                  '${appointment.doctorName}, ${appointment.date}, ${appointment
-                      .time}'),
+                  '${appointment.doctorName}, ${appointment.date}, ${appointment.time}'),
               trailing: appointment.status == 'booked'
                   ? appointment.type == '1'
                   ? Row(
@@ -106,133 +140,143 @@ class _DoctorDashboardScreen extends State<DoctorDashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        backgroundColor: Colors.grey[200],
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          centerTitle: true,
-          elevation: 3,
-          backgroundColor: Colors.grey[200],
-          title: Text('Dashboard'),
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 30),
-                SizedBox(
-                  width: double.infinity,
+    return FutureBuilder<UserProfile?>(
+      future: UserProfile.getUserProfile(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          String? currentuserdisplayname = snapshot.data?.name;
+          return DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              backgroundColor: Colors.grey[200],
+              resizeToAvoidBottomInset: false,
+              appBar: AppBar(
+                centerTitle: true,
+                elevation: 3,
+                backgroundColor: Colors.grey[200],
+                title: Text('Dashboard'),
+                iconTheme: IconThemeData(color: Colors.black),
+              ),
+              body: SafeArea(
+                child: Center(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10.0),
-                        child: Align(
-                          alignment: Alignment.topLeft,
-
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: 12,
-                          right: 12,
-                          top: 5,
-                          bottom: 5,
-                        ),
-                        child: Card(
-                          elevation: 10,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          shadowColor: Colors.grey,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Assigned Patients Namelist",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                                SizedBox(height: 10),
-                                DropdownButton<String>(
-                                  value: selectedEmail,
-                                  items: [
-                                    DropdownMenuItem<String>(
-                                      value: "coomestofcoomer@gmail.com",
-                                      child: Text("coomestofcoomer@gmail.com"),
-                                    ),
-                                    // Add more email options as needed
-                                  ],
-                                  onChanged: (value) {
-                                    setState(() {
-                                      selectedEmail = value!;
-                                    });
-                                  },
-                                ),
-                                SizedBox(height: 10),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context).push(
-                                      MaterialPageRoute(
-                                        builder: (_) => EmotionChart(),
-                                      ),
-                                    );
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    padding: const EdgeInsets.symmetric(horizontal: 30),
-                                    backgroundColor: Colors.purple,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(50),
-                                    ),
-                                  ),
-                                  child: Text(
-                                    "View Patient's Results",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                    ),
-                                  ),
-                                ),
-                              ],
+                      const SizedBox(height: 30),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                              ),
                             ),
-                          ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                left: 12,
+                                right: 12,
+                                top: 5,
+                                bottom: 5,
+                              ),
+                              child: Card(
+                                elevation: 10,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                shadowColor: Colors.grey,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Assigned Patients Namelist",
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                      SizedBox(height: 10),
+                                      DropdownButton<String>(
+                                        value: selectedPatientId,
+                                        items: patientIds.map((String value) {
+                                          return DropdownMenuItem<String>(
+                                            value: value,
+                                            child: Text(value),
+                                          );
+                                        }).toList(),
+                                        onChanged: (value) {
+                                          setState(() {
+                                            selectedPatientId = value!;
+                                          });
+                                        },
+                                      ),
+                                      SizedBox(height: 10),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                              builder: (_) => EmotionChart(),
+                                            ),
+                                          );
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          padding: const EdgeInsets.symmetric(horizontal: 30),
+                                          backgroundColor: Colors.purple,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(50),
+                                          ),
+                                        ),
+                                        child: Text(
+                                          "View Patient's Results",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      TabBar(
+                        labelColor: Colors.black,
+                        tabs: [
+                          Tab(text: 'Upcoming'),
+                          Tab(text: 'Completed'),
+                          Tab(text: 'Canceled'),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Expanded(
+                        child: TabBarView(
+                          children: [
+                            _buildAppointmentsList('booked', currentuserdisplayname ?? "Unknown"),
+                            _buildAppointmentsList('completed', currentuserdisplayname ?? "Unknown"),
+                            _buildAppointmentsList('cancelled', currentuserdisplayname ?? "Unknown"),
+                          ],
                         ),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 30),
-                TabBar(
-                  labelColor: Colors.black,
-                  tabs: [
-                    Tab(text: 'Upcoming'),
-                    Tab(text: 'Completed'),
-                    Tab(text: 'Canceled'),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      _buildAppointmentsList('booked'),
-                      _buildAppointmentsList('completed'),
-                      _buildAppointmentsList('cancelled'),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
--- lib/screens/video_emotion_chart.dart
+++ lib/screens/video_emotion_chart.dart
@@ -1,14 +1,13 @@
-// video_emotion_chart.dart
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fl_chart/fl_chart.dart';
 
 class VideoEmotionChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: fetchData('bharath', 'emotions'), // Replace 'bharath' with the actual username
+      future: _fetchData(), // Fetch data with current user's email
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
@@ -33,10 +32,23 @@ class VideoEmotionChart extends StatelessWidget {
     );
   }
 
-  Future<List<ScatterSpot>> fetchData(String username, String collectionName) async {
+  Future<List<ScatterSpot>> _fetchData() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser == null) {
+      throw Exception("User not logged in");
+    }
+    String? userEmail = currentUser.email;
+
+    var username;
+    if (userEmail == "coomestofcoomer@gmail.com") {
+      username = 'bharath';
+    } else {
+      username = userEmail;
+    }
+
     var querySnapshot = await FirebaseFirestore.instance
         .collection('users')
-        .doc('bharath')
+        .doc(username)
         .collection('emotions')
         .get();
 
@@ -47,10 +59,9 @@ class VideoEmotionChart extends StatelessWidget {
       var timestamp = doc['timestamp'];
 
       if (emotionData != null && timestamp != null) {
-        // Example mapping for converting 'emotion' string to numeric value
-        double calculatedEmotionValue = mapEmotionStringToValue(emotionData);
+        double calculatedEmotionValue = _mapEmotionStringToValue(emotionData);
 
-        double timestampValue = (timestamp as Timestamp).millisecondsSinceEpoch.toDouble() / 1000; // Convert to seconds
+        double timestampValue = (timestamp as Timestamp).millisecondsSinceEpoch.toDouble() / 1000;
 
         dataPoints.add(ScatterSpot(timestampValue, calculatedEmotionValue));
       }
@@ -59,7 +70,7 @@ class VideoEmotionChart extends StatelessWidget {
     return dataPoints;
   }
 
-  double mapEmotionStringToValue(String emotionString) {
+  double _mapEmotionStringToValue(String emotionString) {
     switch (emotionString.toLowerCase()) {
       case 'happy':
         return 1.0;
@@ -80,4 +91,3 @@ class VideoEmotionChart extends StatelessWidget {
     }
   }
 }
-
